3407f6e332ca6e04df6b430df20e7a00ae72774e
soham.pendurkar1 <soham.pendurkar1@olakrutrim.com>
2024-04-30 18:10:59 +0000

    snr calc modify

--- src/run_denoising.sh
+++ src/run_denoising.sh
@@ -6,7 +6,12 @@
 
 source activate denoiser
 
-noisy_dir="/disk2/soham/workspace_old/data_annotation/data/noisy_wavs"
-cleaned_wavs_dir="/disk2/soham/workspace_old/data_annotation/data/cleaned_wavs"
+noisy_dir="/raid/soham.pendurkar/workspace/Voxceleb_data_annotation/data/noisy_wavs"
+cleaned_wavs_dir="/raid/soham.pendurkar/workspace/Voxceleb_data_annotation/data/cleaned_wavs"
 
-python3 -m denoiser.enhance --dns48 --noisy_dir=$noisy_dir --out_dir=$cleaned_wavs_dir
+python3 -m denoiser.enhance \
+            --dns48 \
+            --num_workers 8 \
+            --noisy_dir=$noisy_dir \
+            --out_dir=$cleaned_wavs_dir \
+            --batch_size 32
